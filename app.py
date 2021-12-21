6f99b6883cea346e3f6e143f37da11a1e838daf1
fooooif <qpqh1@naver.com>
2021-12-21 20:52:07 +0900

    기능 추가
    추가, 삭제, 게시판 기능 추가

--- app.py
+++ app.py
@@ -4,11 +4,10 @@ from pymongo import MongoClient
 from flask_jwt_extended import *
 from flask_bcrypt import Bcrypt
 from flask import Flask, render_template, jsonify, request,redirect, url_for
-
+from bson import ObjectId
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 app.config.update(
-    DEBUG = True,
     JWT_SECRET_KEY="my_secret_key_jwt",
     JWT_TOKEN_LOCATION='cookies',
     # get post 뺴고 막아놈
@@ -23,6 +22,7 @@ day = {1:"월",2:"화",3:"수",4:"목",5:"금",6:"토",7:"일"}
 ## HTML 화면 보여주기
 @app.route('/')
 def home():
+
     return render_template('login2.html')
 
 @app.route('/signin' ,methods=['GET'])
@@ -43,7 +43,11 @@ def board():
     jwt_token=get_jwt_identity()
     email = jwt_token['email']
     name = db.users.find_one({'email':email})['name']
-    return render_template('board.html',name = name)
+    boards = list(db.boards.find({'email': email}))
+    for board in boards:
+        board['_id']=str(board['_id'])
+
+    return render_template('board.html',name = name,board_list=boards)
 
 ## api 메소드 -> request.form -> 안받아짐 체크해보기
 # user 회원가입.
@@ -94,7 +98,7 @@ def login():
         json = {
             'email':email,
         }
-        access_token = create_access_token(identity=json,expires_delta=datetime.timedelta(minutes=15))
+        access_token = create_access_token(identity=json,expires_delta=datetime.timedelta(minutes=60))
         return jsonify({'msg':'success','access_token':access_token})
     else:
         return jsonify({'msg':'fail','access_token':None})
@@ -105,12 +109,40 @@ def login():
 def register_board():
     jwt_token = get_jwt_identity()
     email = jwt_token['email']
-    name = db.users.find_one({'email': email})['name']
-    print(request.is_json)
-    print(request.data)
+    json = request.get_json()['data']
+    days = []
+    for i in json['day']:
+        days.append(day[i])
+    board_doc = {
+        'email':email,
+        'day':days,
+        'title':json['title'],
+        'content':json['content'],
+        'time':json['time']
+    }
+    db.boards.insert_one(board_doc)
+
+    return jsonify({'msg':'succes'})
+@app.route('/api/board/drop',methods=['POST'])
+@jwt_required()
+def delete_board():
+    jwt_token = get_jwt_identity()
+    json = request.get_json()
+    board_ids = json['id']
+
+    for board_id in board_ids:
+        db.boards.delete_one({'_id':ObjectId(board_id)})
     return jsonify({'msg':'succes'})
 
-
+# @app.route('/api/board',methods=['GET'])
+# @jwt_required()
+# def find_board():
+#     jwt_token = get_jwt_identity()
+#     email = jwt_token['email']
+#
+#     boards = list(db.boards.find({'email': email}, {'_id': False}))
+#
+#     return jsonify({'data':boards})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
--- db_init.py
+++ db_init.py
@@ -4,20 +4,39 @@ client = MongoClient('localhost', 27017)
 db = client.scheduler
 
 
-def db_users():
-    user_doc = {
-        "name": "",
-        "email": "",
-        "password": "",
-    }
-    db.users.insert_one(user_doc)
-
-
-def db_board():
-    board_doc = {
-        "user_name": "",
-        "password": "",
-        "title": "",
-        "content":""
-    }
-    db.boards.insert_one(board_doc)
+#
+# def db_users():
+#     user_doc = {
+#         "name": "",
+#         "email": "",
+#         "password": "",
+#     }
+#     db.users.insert_one(user_doc)
+#
+#
+# def db_board():
+#     board_doc = {
+#         'email':"",
+#         'day': [],
+#         'title': "",
+#         'content': "",
+#         'time': ""
+#     }
+#     db.boards.insert_one(board_doc)
+#
+#
+# # 저장 - 예시
+# doc = {'name':'bobby','age':21}
+# db.users.insert_one(doc)
+#
+# # 한 개 찾기 - 예시
+# user = db.users.find_one({'name':'bobby'})
+#
+# # 여러개 찾기 - 예시 ( _id 값은 제외하고 출력)
+# same_ages = list(db.users.find({'age':21},{'_id':False}))
+#
+# # 바꾸기 - 예시
+# db.users.update_one({'name':'bobby'},{'$set':{'age':19}})
+#
+# # 지우기 - 예시
+# db.users.delete_one({'name':'bobby'})
